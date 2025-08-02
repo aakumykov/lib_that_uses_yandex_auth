f593b5621befcf5a6ddee4a2ac17583d1cf2acd6
Andrey Kumykov <aakumykov@yandex.ru>
2025-08-02 18:00:19 +0300

    v0.0.1

---
+++ app/src/test/java/com/github/aakumykov/lib_that_uses_yandex_auth_demo/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.github.aakumykov.lib_that_uses_yandex_auth_demo
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
